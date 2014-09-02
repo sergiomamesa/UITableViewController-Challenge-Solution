8fbc411fd6a9e9d2954ab409fb63e2fc5b040b63
Irene Santamaría de Pedraza <Irene@MacBook-Pro-de-Irene.local>
2014-09-02 20:13:29 +0200

    Solved the challenge

--- UITableViewController Challenge Solution/CSTableViewController.h
+++ UITableViewController Challenge Solution/CSTableViewController.h
@@ -1,13 +1,13 @@
 //
-//  CSViewController.h
+//  CSTableViewController.h
 //  UITableViewController Challenge Solution
 //
-//  Created by Irene Santamaría de Pedraza on 01/09/14.
+//  Created by Irene Santamaría de Pedraza on 02/09/14.
 //  Copyright (c) 2014 Sergio Magaña. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface CSViewController : UIViewController
+@interface CSTableViewController : UITableViewController
 
 @end
